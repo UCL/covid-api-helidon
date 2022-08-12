21ab03b5dd4240275c4b08e3ba9c2d00e86def82
David Guzman <d.guzman@ucl.ac.uk>
2022-08-12 19:27:00 +0100

    WIP add entities for model data

---
+++ src/main/java/uk/ac/ucl/cs/covid/persistence/ModelEntity.java
@@ -0,0 +1,31 @@
+package uk.ac.ucl.cs.covid.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PUBLIC.MODEL")
+public class ModelEntity {
+  @Id
+  private Integer id;
+
+  @Column(name = "MODEL_NAME", nullable = false)
+  private String modelName;
+
+  @Column(name="IS_DISPLAYED", nullable = false)
+  private Boolean isDisplayed;
+
+  @Column(name="COUNTRY_ISO_A3", nullable = false)
+  private String countryIsoA3;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+}
--- src/main/java/uk/ac/ucl/cs/covid/persistence/ModelScoresEntity.java
+++ src/main/java/uk/ac/ucl/cs/covid/persistence/ModelScoresEntity.java
@@ -1,8 +1,40 @@
 package uk.ac.ucl.cs.covid.persistence;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "PUBLIC.MODEL_SCORES")
 public class ModelScoresEntity {
+
+  @Id
+  private Integer id;
+
+  @Column(name = "IMPORt_tiMESTAMP", nullable = false)
+  private LocalDateTime importTimestamp;
+
+  @Column(name = "SCORE_DATE", nullable = false)
+  private LocalDate scoreDate;
+
+  @Column(name = "WEIGHTED_DEBIASED", nullable = false)
+  private Double weightedDebiased;
+
+  @Column(nullable = false)
+  private Double weighted;
+
+  @Column(name = "HISTORICAL_TREND", nullable = false)
+  private Double historicalTrend;
+
+  @Column(name = "HISTORICAL_TREND_LOWER", nullable = false)
+  private Double historicalTrendLower;
+
+  @Column(name = "HISTORICAL_TREND_UPPER", nullable = false)
+  private Double historicalTrendUpper;
+
 
 }
